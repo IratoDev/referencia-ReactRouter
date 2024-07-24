3ea5221e78acb3055de1530e708265157954dccd
IratoDev <iratodev.frontend@gmail.com>
2024-07-24 11:04:37 -0300

    versão 1 finalizada

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,31 +1,70 @@
-import { useEffect , useState} from "react";
+import { useEffect , useState, useReducer} from "react";
 import { Link,useLocation } from "react-router-dom";
 import "./StyleNavBar.modules.css"
 
 export default function NavBar(){
 
-
-const[Button, setButton]=useState(false);
-const[Produto, setProduto] = useState();
+const[Produto, setProduto] = useState("");
 const location = useLocation();
+const reducer =(State, action)=>{
 
+  switch(action.type){
+  
+  case "Home":
+  return{ativo:"Home"}
+  
+  case "Sobre":
+  return{ativo:"Sobre"}
+  
+  case "Produto":
+  return{ativo:"Produto"}
+  
+  default:
+  return{ativo:"Error"}
+  
+  
+  }
+  
+}
+
+const [State, dispatch] = useReducer(reducer,{ativo:"Home"});
 
 useEffect(() => {
-    if (location.pathname === "/Sobre") {
-      setButton(true);
-    } else {
-      setButton(false);
-    }
+
+  if (location.pathname === "/") {
+    dispatch({ type: "Home" });
+  } else if (location.pathname === "/Sobre") {
+    dispatch({ type: "Sobre" });
+  } else if (location.pathname.startsWith("/produto")) {
+    dispatch({ type: "Produto" });
+  } else {
+    dispatch({ type: "Error" });
+  }
+
 }, [location.pathname]);
 
 
+const getBackgroundColor = (status) => {
+  switch (status) {
+    case "Home":
+      return "#045cff";
+    case "Sobre":
+      return "#f79f1d";
+    case "Produto":
+      return "#003811"; // Exemplo de cor para Produto
+    case "Error":
+      return "#2b0500"; // Exemplo de cor para Error
+    default:
+      return "#FFF"; // Cor padrão se necessário
+  }
+};
 
 const StyleButton = {
 
 padding: "1em",
 borderRadius: "20px",
-backgroundColor: (Button ? "#f79f1d":"#045cff" ),
-color: "#000",
+backgroundColor: getBackgroundColor(State.ativo),
+color: "#292828",
 
 }
 
@@ -39,6 +78,7 @@ setProduto(ProdutoID)
 
 }
 
+console.log(State)
 console.log(Produto)
 
 return(
